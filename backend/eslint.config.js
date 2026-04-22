ebdcb95750920a19661e9768c7b8282ee8d43889
Jason Blackschleger <jay@macbookair.home>
2026-04-21 20:29:21 -0500

    removed imports eslint

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -1,10 +1,5 @@
-import globals from "globals";
-import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
-
 export default [
   {
     files: ["**/*.{js,mjs,cjs,jsx}"],
-    languageOptions: { globals: { process: "readonly", console: "readonly" } },
   },
 ];
